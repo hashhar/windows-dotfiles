96a18acff9be1692d57d58b8c8794157738d7756
Ashhar Hasan <hashhar_dev@outlook.com>
2017-02-12 00:43:41 +0530

    Add function to check if something exists in PATH

--- PowerShell/profile.ps1
+++ PowerShell/profile.ps1
@@ -1,10 +1,25 @@
+function Add-To-Path($path) {
+    $pathSplit = $env:Path.Split(';')
+    foreach ($item in $pathSplit) {
+        if ($item.TrimEnd('\').Equals($path.TrimEnd('\'))) {
+            Write-Output "Already in PATH."
+            break
+        }
+    }
+    $env:Path.Insert(0, $path)
+}
+
 function Use-SSH () {
-    $env:Path = $env:ProgramFiles + "\Git\usr\bin;" + $env:Path
+    $sshPath = $env:ProgramFiles + "\Git\usr\bin"
+    Add-To-Path($sshPath)
     Start-SshAgent
 }
 
 function Use-SystemPython () {
-    $env:Path = $env:HOMEDRIVE + "\Python27;" + $env:HOMEDRIVE + "\Python27\Scripts;" + $env:Path
+    $pythonPath = $env:HOMEDRIVE + "\Python27"
+    $pythonScriptPath = $pythonPath + "\Scripts"
+    Add-To-Path($pythonPath)
+    Add-To-Path($pythonScriptPath)
     Write-Output "Using system Python2 now."
 }
 
