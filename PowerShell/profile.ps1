dfb67ae301a413375e5462c275bbde46a39be4f8
Ashhar Hasan <hashhar_dev@outlook.com>
2017-09-03 18:37:51 +0530

    Add: Function to find git repos and extract remotes

--- PowerShell/profile.ps1
+++ PowerShell/profile.ps1
@@ -16,21 +16,12 @@ function Use-SSH () {
     Add-To-Path($sshPath)
     Start-SshAgent
     # Replace the keyname with the keys you want to add.
-    ssh-add "$env:HOME\.ssh\id_rsa_github"
+    ssh-add "$env:HOME\.ssh\id_rsa"
     # Uncomment below lines to add all keys on startup (is irritating).
     # $keys = Get-ChildItem -Path "$env:HOME\.ssh\" -Filter "*.pub"
     # foreach ($key in $keys) {
     #     ssh-add $key.FullName.TrimEnd(".pub")
     # }
-}
-
-function Use-SystemPython () {
-    $pythonPath = $env:HOMEDRIVE + "\Python27"
-    $pythonScriptPath = $pythonPath + "\Scripts"
-    Add-To-Path($pythonPath)
-    Add-To-Path($pythonScriptPath)
-    $env:PYTHON = $pythonPath + "\python.exe"
-    Write-Output "Using system Python2 now."
 }
 
 function global:prompt {
@@ -42,9 +33,93 @@ function global:prompt {
     return "> "
 }
 
-# Set up gitattributes and gitignore and GitHub aliases.
-$github = "D:\GitHub"
-$gitatt = $github + "\_ToUse\gitattributes"
-$gitig = $github + "\_ToUse\gitignore"
+function Find-GitRepository {
+    <#
+    .SYNOPSIS
+    Find Git repositories
+    .DESCRIPTION
+    Use this command to find Git repositories in the specified folder. It is assumed that you have the Git command line tools already installed.
+    .PARAMETER Path
+    The top level path to search.
+    .INPUTS
+    [string]
+    .OUTPUTS
+    [pscustomobject]
+    #>
+
+    [cmdletbinding()]
+    Param(
+        [Parameter(
+            Position = 0,
+            HelpMessage = "The top level path to search"
+        )]
+        [ValidateScript( {
+                if (Test-Path $_) {
+                    $True
+                }
+                else {
+                    Throw "Cannot validate path $_"
+                }
+            })]
+        [string]$Path = "."
+    )
+
+    Write-Verbose "[BEGIN  ] Starting: $($MyInvocation.Mycommand)"
+    Write-Verbose "[PROCESS] Searching $(Convert-Path -path $path) for Git repositories"
+    $cwd = Get-Location
+
+    Get-ChildItem -path $Path -Hidden -filter .git -Recurse |
+        Select-Object @{Name = "Repository"; Expression = {Convert-Path $_.PSParentPath}},
+    @{Name = "Branch"; Expression = {
+            #save current location
+            Push-Location
+            #change location to the repository
+            Set-Location -Path (Convert-Path -path ($_.psparentPath))
+            #get current branch with out the leading asterisk
+            # (git branch).where( {$_ -match "\*"}).Substring(2)
+            # Use posh-git's util functions
+            Get-GitBranch
+        }
+    },
+    @{Name = "Remote"; Expression = {
+            $remotes = Read-GitRemotesWithUrl .git/config
+            foreach ($remote in $remotes.Keys) {
+                if ($remote.Equals("NO_SECTION")) {
+                    continue
+                }
+                $remotes[$remote]['url']
+            }
+            #change back to original location
+            Pop-Location
+        }
+    }
+
+    Write-Verbose "[END    ] Ending: $($MyInvocation.Mycommand)"
+    Set-Location $cwd
+
+} #end function
+
+Function Read-GitRemotesWithUrl ($file) {
+    $ini = @{}
+
+    # Create a default section if none exist in the file. Like a java prop file.
+    $section = "NO_SECTION"
+    $ini[$section] = @{}
+
+    switch -regex -file $file {
+        '^\[(remote "(.+)")\]$' {
+            $section = $matches[2].Trim()
+            $ini[$section] = @{}
+        }
+        "^\s*(url)\s*=\s*(.*)" {
+            $name, $value = $matches[1..2]
+            # skip comments that start with semicolon:
+            if (!($name.StartsWith(";"))) {
+                $ini[$section][$name] = $value.Trim()
+            }
+        }
+    }
+    $ini
+}
 
 Import-Module posh-git
